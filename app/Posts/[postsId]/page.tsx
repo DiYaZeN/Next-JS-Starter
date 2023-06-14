deb0402951f14c4d6b280a8fc4e707c8de44f711
DiYaZeN <minolamarakoon@gmail.com>
2023-06-14 15:53:41 +0530

    1

---
+++ app/Posts/[postsId]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+const postId="";
+const page = () => {
+  return (
+    <div>{ postId}</div>
+  )
+}
+
+export default page  
---
+++ app/Posts/new/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Page = () => { 
+    return (
+        <div>
+        <h1>New Post</h1>
+        </div>
+    ); 
+}
+export default Page; 
